a436c6b16671408ccc684f84a6c726a71c58ca71
Jorge Ulises Useche Cuellar <naturalmentejorge@gmail.com>
2015-08-15 19:56:51 -0500

    Comentarios a Estilo.php y Script.php

--- blocks/gui/menuPrincipal/css/Estilo.php
+++ blocks/gui/menuPrincipal/css/Estilo.php
@@ -1,4 +1,20 @@
 <?php
+/*
+ * Este bloque necesita bootstrap.css para eso en la página escriba 
+ * bootstrap=true
+ * ó bootstrap=3.3.5 
+ * ó bootstrap=3.3.5.min 
+ * o alguna otra versión que considere correspondiente
+ */
+
+/*
+ * Este bloque necesita bootstrap para eso en la página escriba en la tabla de páginas en la columna "parametro"
+ * bootstrap=true
+ * ó bootstrap=3.3.5
+ * ó bootstrap=3.3.5.min
+ * o alguna otra versión que considere correspondiente
+ */
+
 // $indice = 0;
 
 // $estilo [$indice++] = "bootstrap.min.css";
--- blocks/gui/menuPrincipal/script/Script.php
+++ blocks/gui/menuPrincipal/script/Script.php
@@ -7,14 +7,19 @@
  * ready.js o ready.php. DICHOS ARCHIVOS DEBEN IR EN LA CARPETA script DE LOS BLOQUES PERO NO RELACIONARSE AQUI. 
  */
 
+/*
+ * Este bloque necesita bootstrap para eso en la página escriba en la tabla de páginas en la columna "parametro"
+ * bootstrap=true
+ * ó bootstrap=3.3.5
+ * ó bootstrap=3.3.5.min
+ * o alguna otra versión que considere correspondiente
+ */
+
 // Registrar los archvos js que deben incluirse
 
 //No son necesarios los scripts para el menú porque en los links de abajo se carga.
 // $indice=0;
 // $funcion[$indice++]='bootstrap.js';
-
-
-
 
 // $rutaBloque=$this->miConfigurador->getVariableConfiguracion("host");
 // $rutaBloque.=$this->miConfigurador->getVariableConfiguracion("site");
